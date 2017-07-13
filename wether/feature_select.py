f26ad9380a7411dcb4ba9e7153d6f8b77b27e910
wangyy <862552658@qq.com>
2017-07-13 17:04:59 +0800

    进行天气的获取和天气数据的处理

--- wether/feature_select.py
+++ wether/feature_select.py
@@ -25,57 +25,95 @@ info = cur.fetchmany(aag)
 cur.close()
 conn.commit()
 conn.close()
-t = 3
+
+def getWeatherFeature():
+    conn = MySQLdb.connect(host='localhost', user=user1, passwd=password1, db=mysqlDB, port=3306, charset='utf8')
+    cur = conn.cursor()
+    sql = 'SELECT * from weather where shi="杭州"'
+    weather = pd.read_sql(sql, conn, index_col='time')
+    aag = cur.execute('SELECT * from weather where shi="杭州"')
+    info = cur.fetchmany(aag)
+    cur.close()
+    conn.commit()
+    conn.close()
+
+    t = 3
+    feathre = []
+    values = []
+    for idx in weather.index:  # 前后三天
+        all_feature={}
+        # if datetime.datetime(2011, 1, 4) <= idx <= datetime.datetime(2016, 12, 28):
+        if idx == datetime.datetime(2016, 12, 28):
+            weather_z = {}
+            wind1_z = {}
+            wind2_z = {}
+            temp_z = {}
+            d = 2*t+1
+            day_night = {}
+            temp_day_day = {}
+            temp_night_night = {}
+            temp_day_night = {}
+            temp_night_day = {}
+            for i in range(1,d+1):
+                forward = idx - datetime.timedelta(days=t) + datetime.timedelta(days=i-1)  # 七天时间
+
+                weahter1 = {"weather_day" + str(i): weather.loc[forward]['weather_day'],"weather_night" + str(i): weather.loc[forward]['weather_night']}
+                weather_z = dict(weather_z, **weahter1)
+
+                wind1 = {"wind_day1" + str(i): weather.loc[forward]['wind_day1'],"wind_night1" + str(i): weather.loc[forward]['wind_night1']}
+                wind1_z = dict(wind1_z, **wind1)
+
+                wind2 = {"wind_day2" + str(i): weather.loc[forward]['wind_day2'],"wind_night2" + str(i): weather.loc[forward]['wind_night2']}
+                wind2_z = dict(wind2_z, **wind2)
+
+                temp = {"tem_day" + str(i): weather.loc[forward]['tem_day'],"tem_night" + str(i): weather.loc[forward]['tem_night']}
+                temp_z = dict(temp_z, **temp)
+
+                day_day1={"day" + str(i)+":day"+str(j+1): weather.loc[forward]['tem_day'] - weather.loc[forward + datetime.timedelta(days=j)]['tem_day'] for j in range(1,d+1-i) }  # 日温差
+                temp_day_day=dict(temp_day_day,**day_day1)
+
+                night_night1= {"night" + str(i)+":next_night"+str(i+j): weather.loc[forward]['tem_night'] -weather.loc[forward + datetime.timedelta(days=j)]['tem_night'] for j in range(1,d+1-i) }   # 夜温差
+                temp_night_night = dict(temp_night_night,**night_night1)
+
+                day_night1={"day" + str(i)+":night"+str(i+j): weather.loc[forward]['tem_day'] - weather.loc[forward+ datetime.timedelta(days=j)]['tem_night'] for j in range(0,d-i)}   # 日夜温差
+                temp_day_night=dict(temp_day_night,**day_night1)
+
+                night_day1={"night" + str(i)+":next_day"+str(i+j): weather.loc[forward]['tem_night'] -weather.loc[forward + datetime.timedelta(days=j)]['tem_day'] for j in range(1,d+1-i)}  # 大量日夜温差  1-7天
+                temp_night_day=dict(temp_night_day,**night_day1)
+
+            all_feature=dict(weather_z.items()+wind1_z.items()+wind2_z.items()+temp_z.items()+temp_day_day.items()+temp_night_night.items()+temp_day_night.items()+temp_night_day.items())          #多个字段字典合并
+            feathre = all_feature.keys()
+            value = all_feature.values()
+            values.append(value)
+
+        else:
+            continue
+
+    all_values = pd.DataFrame(values,columns=feathre)
+    all_values = all_values.concat(weather)
+
+hangzhou_date = weather[[5]]
+
 feathre = []
 values = []
-for idx in weather.index:  # 前后三天
-    all_feature={}
-    # if datetime.datetime(2011, 1, 4) <= idx <= datetime.datetime(2016, 12, 28):
-    if idx == datetime.datetime(2016, 12, 28):
-        weather_z = {}
-        wind1_z = {}
-        wind2_z = {}
-        temp_z = {}
-        d = 2*t+1
-        day_night = {}
-        temp_day_day = {}
-        temp_night_night = {}
-        temp_day_night = {}
-        temp_night_day = {}
-        for i in range(1,d+1):
-            forward = idx - datetime.timedelta(days=t) + datetime.timedelta(days=i-1)  # 七天时间
+times = []
+for idx in hangzhou_date.index:  # 前7天 后三天
+    if datetime.datetime(2011, 1, 10) <= idx <= datetime.datetime(2016, 12, 20):
+        d = 10
+        disease_z = {}
+        time = {"time": idx}
+        for i in range(0, d):
+            forward = idx - datetime.timedelta(days=(7 - i))  # 七天时间
+            try:
+                infection = {"disease_day" + str(i - 7): hangzhou_date.loc[forward]['tem_night']}
+            except:
+                infection = {"disease_day" + str(i - 7): hangzhou_date.loc[forward]['tem_night']}
+            disease_z = dict(disease_z, **infection)
 
-            weahter1 = {"weather_day" + str(i): weather.loc[forward]['weather_day'],"weather_night" + str(i): weather.loc[forward]['weather_night']}
-            weather_z = dict(weather_z, **weahter1)
-
-            wind1 = {"wind_day1" + str(i): weather.loc[forward]['wind_day1'],"wind_night1" + str(i): weather.loc[forward]['wind_night1']}
-            wind1_z = dict(wind1_z, **wind1)
-
-            wind2 = {"wind_day2" + str(i): weather.loc[forward]['wind_day2'],"wind_night2" + str(i): weather.loc[forward]['wind_night2']}
-            wind2_z = dict(wind2_z, **wind2)
-
-            temp = {"tem_day" + str(i): weather.loc[forward]['tem_day'],"tem_night" + str(i): weather.loc[forward]['tem_night']}
-            temp_z = dict(temp_z, **temp)
-
-            day_day1={"day" + str(i)+":day"+str(j+1): weather.loc[forward]['tem_day'] - weather.loc[forward + datetime.timedelta(days=j)]['tem_day'] for j in range(1,d+1-i) }  # 日温差
-            temp_day_day=dict(temp_day_day,**day_day1)
-
-            night_night1= {"night" + str(i)+":next_night"+str(i+j): weather.loc[forward]['tem_night'] -weather.loc[forward + datetime.timedelta(days=j)]['tem_night'] for j in range(1,d+1-i) }   # 夜温差
-            temp_night_night = dict(temp_night_night,**night_night1)
-
-            day_night1={"day" + str(i)+":night"+str(i+j): weather.loc[forward]['tem_day'] - weather.loc[forward+ datetime.timedelta(days=j)]['tem_night'] for j in range(0,d-i)}   # 日夜温差
-            temp_day_night=dict(temp_day_night,**day_night1)
-
-            night_day1={"night" + str(i)+":next_day"+str(i+j): weather.loc[forward]['tem_night'] -weather.loc[forward + datetime.timedelta(days=j)]['tem_day'] for j in range(1,d+1-i)}  # 大量日夜温差  1-7天
-            temp_night_day=dict(temp_night_day,**night_day1)
-
-        all_feature=dict(weather_z.items()+wind1_z.items()+wind2_z.items()+temp_z.items()+temp_day_day.items()+temp_night_night.items()+temp_day_night.items()+temp_night_day.items())          #多个字段字典合并
-        feathre = all_feature.keys()
-        value = all_feature.values()
+        feathre = disease_z.keys()
+        value = disease_z.values()
         values.append(value)
-
     else:
         continue
-
-all_values = pd.DataFrame(values,columns=feathre)
-all_values = all_values.concat(weather)
+disease_values = pd.DataFrame(values, columns=feathre)
+disease_values.head()
--- wether/web_weather.py
+++ wether/web_weather.py
@@ -28,7 +28,7 @@ mysqlDB="sys"
 user1="root"
 password1='sampson'
 import calendar
-
+import pinyin
 # 先调用无界面浏览器PhantomJS或Firefox
 # driver = webdriver.PhantomJS()
 driver = webdriver.Chrome(executable_path=r"E:\DATA\chromedriver_win32\chromedriver.exe")
@@ -45,9 +45,11 @@ def GetSearchContent(shi_qu,months):
         citys = driver.find_element_by_xpath('//*[@class="citychk"]/dl['+str(ci)+']/dd').text
         citys = citys.split(" ")
         city_first = citys[0]
+        city_first=pinyin.get(city_first, format='strip', delimiter="")
         for i in range(1,len(citys)+1):  #获取每个市的地级县区
             driver.get("http://www.tianqihoubao.com/lishi/zhejiang.htm")
             shi  = driver.find_element_by_xpath('//*[@class="citychk"]/dl['+str(ci)+']/dd/a['+str(i)+']').text
+            shi=pinyin.get(shi, format='strip', delimiter="")
             href = driver.find_element_by_xpath('//*[@class="citychk"]/dl['+str(ci)+']/dd/a['+str(i)+']').get_attribute("href")
             href = href.split(".html")[0]
             for k in range(1,months+1):       #60个月 2011-2016
@@ -70,8 +72,10 @@ def GetSearchContent(shi_qu,months):
                     try:
                         day_time    = driver.find_element_by_xpath('//*[@id="content"]/table/tbody/tr['+str(e)+']/td[1]/a').text
                         wether      = driver.find_element_by_xpath('//*[@id="content"]/table/tbody/tr['+str(e)+']/td[2]').text
-                        weather_day      = wether.split("/")[0]
-                        weather_night    = wether.split("/")[1]
+                        weather_day   = wether.split("/")[0]
+                        weather_day   = pinyin.get(weather_day, format='strip', delimiter="")
+                        weather_night = wether.split("/")[1]
+                        weather_night = pinyin.get(weather_night, format='strip', delimiter="")
                         temperature = driver.find_element_by_xpath('//*[@id="content"]/table/tbody/tr['+str(e)+']/td[3]').text
                         tem_day     = temperature.split("/")[0]
                         tem_day     = int(filter(lambda ch: ch in '0123456789-', tem_day))
@@ -80,16 +84,20 @@ def GetSearchContent(shi_qu,months):
                         wind        = driver.find_element_by_xpath('//*[@id="content"]/table/tbody/tr['+str(e)+']/td[4]').text
                         wind_day    = wind.split("/")[0]
                         wind_day1   = wind_day.split(" ")[0]
+                        wind_day1   = pinyin.get(wind_day1, format='strip', delimiter="")
                         wind_day2   = wind_day.split(" ")[1]
+                        wind_day2   = pinyin.get(wind_day2, format='strip', delimiter="")
                         wind_night  = wind.split("/")[1]
                         wind_night1 = wind_night.split(" ")[0]
-                        wind_night2 =wind_night.split(" ")[1]
+                        wind_night1 = pinyin.get(wind_night1, format='strip', delimiter="")
+                        wind_night2 = wind_night.split(" ")[1]
+                        wind_night2 = pinyin.get(wind_night2, format='strip', delimiter="")
                     except:
                         continue
                     values.append((city_first.encode('UTF-8'),shi.encode('UTF-8'),day,weather_day.encode('UTF-8'),weather_night.encode('UTF-8'),tem_day,tem_night,wind_day1.encode('UTF-8'),wind_day2.encode('UTF-8'),wind_night1.encode('UTF-8'),wind_night2.encode('UTF-8')))
                 conn = MySQLdb.connect(host='localhost', user=user1, passwd=password1, db=mysqlDB, port=3306,charset="utf8")
                 cur = conn.cursor()
-                cur.executemany('insert into weather values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)', values)
+                cur.executemany('insert into weather2 values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)', values)
                 conn.commit()
                 cur.close()
                 conn.close()
@@ -98,7 +106,7 @@ if __name__ == '__main__':
     months = 72
     conn = MySQLdb.connect(host='localhost', user=user1, passwd=password1, db=mysqlDB, port=3306,charset="utf8")
     cur = conn.cursor()
-    cur.execute('create table weather (city_first varchar(300),shi varchar(300),time datetime,weather_day varchar(300),weather_night varchar(300),tem_day int,tem_night int,wind_day1 varchar(300),wind_day2 varchar(300),wind_night1 varchar(300),wind_night2 varchar(300))')
+    cur.execute('create table weather2 (city_first varchar(300),shi varchar(300),time datetime,weather_day varchar(300),weather_night varchar(300),tem_day int,tem_night int,wind_day1 varchar(300),wind_day2 varchar(300),wind_night1 varchar(300),wind_night2 varchar(300))')
     conn.commit()
     cur.close()
     conn.close()
